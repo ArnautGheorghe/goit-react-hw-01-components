d48c57758731b8c600d12afdc38747e9e07a29cf
ArnautGheorghe <agigi94@yahoo.com>
2024-01-17 21:02:52 +0200

    update 1

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,9 @@
+import styles from "./Profile.module.css"
+import data from "./user.json"
+
+const Profile = () => {
+    console.log(data)
+return <div> Profile page </div>
+}
+
+export default Profile
