4d6e51373f0ec04b688ae360ed1964694a260b55
ArnautGheorghe <agigi94@yahoo.com>
2024-01-23 09:11:08 +0200

    tema 1 pc2

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,9 @@
+import styles from "./Statistics.module.css";
+import data from './data.json';
+
+const Statistics = () => {
+  console.log(data);
+  return <div> Statistics page </div>;
+};
+
+export default Statistics;
